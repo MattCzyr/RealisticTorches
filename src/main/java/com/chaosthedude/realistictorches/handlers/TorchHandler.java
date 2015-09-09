28adf46d7b07e1d28c0682c90d8852bccf44a1b0
ChaosTheDude <chaos.realm@yahoo.com>
2015-09-08 20:27:08 -0400

    Fix torches not being removed from inventory after being placed, refine method for determining if the location is liquid

--- src/main/java/com/chaosthedude/realistictorches/handlers/TorchHandler.java
+++ src/main/java/com/chaosthedude/realistictorches/handlers/TorchHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.block.BlockLiquid;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 import com.chaosthedude.realistictorches.blocks.BlockRegistry;
@@ -20,46 +21,70 @@ public class TorchHandler {
 		if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
 			if (player.getCurrentEquippedItem() != null) {
 				if (player.getCurrentEquippedItem().getItem() == Item.getItemFromBlock(Blocks.torch)) {
-					event.useBlock = Result.DENY;
 					
 					if (event.world.isBlockNormalCubeDefault(event.x, event.y, event.z, true) == true) {
 						
-						if (event.world.getBlock(event.x, event.y, event.z) instanceof BlockLiquid ||
+						if (event.face == 1 && (event.world.getBlock(event.x, event.y + 1, event.z) instanceof BlockLiquid ||
+								event.world.getBlock(event.x + 1, event.y + 1, event.z) instanceof BlockLiquid ||
+								event.world.getBlock(event.x - 1, event.y + 1, event.z) instanceof BlockLiquid ||
+								event.world.getBlock(event.x, event.y + 1, event.z + 1) instanceof BlockLiquid ||
+								event.world.getBlock(event.x, event.y + 1, event.z - 1) instanceof BlockLiquid ||
+								event.world.getBlock(event.x, event.y + 2, event.z) instanceof BlockLiquid)) {
+							event.setCanceled(true);
+						}
+						
+						else  if (event.face > 1 && (event.world.getBlock(event.x, event.y, event.z) instanceof BlockLiquid ||
 								event.world.getBlock(event.x + 1, event.y, event.z) instanceof BlockLiquid ||
 								event.world.getBlock(event.x - 1, event.y, event.z) instanceof BlockLiquid ||
 								event.world.getBlock(event.x, event.y, event.z + 1) instanceof BlockLiquid ||
 								event.world.getBlock(event.x, event.y, event.z - 1) instanceof BlockLiquid ||
-								event.world.getBlock(event.x, event.y + 1, event.z) instanceof BlockLiquid) {
+								event.world.getBlock(event.x, event.y + 1, event.z) instanceof BlockLiquid)) {
 							event.setCanceled(true);
 						}
 						
 						else if (event.face == 1) {
 							if (!event.world.isRemote) {
 								event.world.setBlock(event.x, event.y + 1, event.z, BlockRegistry.torchLit, 5, 2);
+								if (!player.capabilities.isCreativeMode) {
+									player.inventory.consumeInventoryItem(Item.getItemFromBlock(Blocks.torch));
+								}
 							}
 						}
 					
 						else if (event.face == 2) {
 							if (!event.world.isRemote) {
 								event.world.setBlock(event.x, event.y, event.z - 1, BlockRegistry.torchLit, 4, 2);
+								if (!player.capabilities.isCreativeMode) {
+									player.inventory.consumeInventoryItem(Item.getItemFromBlock(Blocks.torch));
+								}
 							}
 						}
 					
 						else if (event.face == 3) {
 							if (!event.world.isRemote) {
 								event.world.setBlock(event.x, event.y, event.z + 1, BlockRegistry.torchLit, 3, 2);
+								if (!player.capabilities.isCreativeMode) {
+									player.inventory.consumeInventoryItem(Item.getItemFromBlock(Blocks.torch));
+								}
 							}
 						}
 					
 						else if (event.face == 4) {
 							if (!event.world.isRemote) {
 								event.world.setBlock(event.x - 1, event.y, event.z, BlockRegistry.torchLit, 2, 2);
+								if (!player.capabilities.isCreativeMode) {
+									player.inventory.consumeInventoryItem(Item.getItemFromBlock(Blocks.torch));
+								}
 							}
 						}
 					
 						else if (event.face == 5) {
 							if (!event.world.isRemote) {
 								event.world.setBlock(event.x + 1, event.y, event.z, BlockRegistry.torchLit, 1, 2);
+								ItemStack stack = player.getCurrentEquippedItem();
+								if (!player.capabilities.isCreativeMode) {
+									player.inventory.consumeInventoryItem(Item.getItemFromBlock(Blocks.torch));
+								}
 							}
 						}
 					
