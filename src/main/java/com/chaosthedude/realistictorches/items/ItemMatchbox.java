3212956bcb2f6325c0cc61bd2542f807af443441
ChaosTheDude <chaos.realm@yahoo.com>
2015-10-25 19:18:13 -0400

    Added config option for unlimited matchbox uses

--- src/main/java/com/chaosthedude/realistictorches/items/ItemMatchbox.java
+++ src/main/java/com/chaosthedude/realistictorches/items/ItemMatchbox.java
@@ -1,51 +1,56 @@
 package com.chaosthedude.realistictorches.items;
 
+import com.chaosthedude.realistictorches.RealisticTorches;
+import com.chaosthedude.realistictorches.handlers.ConfigHandler;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-import com.chaosthedude.realistictorches.RealisticTorches;
-import com.chaosthedude.realistictorches.handlers.ConfigHandler;
-
 public class ItemMatchbox extends Item {
-	
+
 	private ItemStack emptyItem = null;
-	
+
 	public static final String name = "Matchbox";
-	
+
 	public ItemMatchbox() {
 		super();
-		
+
 		this.setUnlocalizedName(RealisticTorches.ID + "_" + name);
 		this.maxStackSize = 1;
-        this.setMaxDamage(ConfigHandler.matchboxDurability - 1);
+		this.setMaxDamage(ConfigHandler.matchboxDurability - 1);
 		this.setCreativeTab(CreativeTabs.tabTools);
 		this.setTextureName(RealisticTorches.ID + ":" + name);
+		this.setNoRepair();
 	}
-	
+
 	@Override
 	public boolean hasContainerItem() {
 		return true;
 	}
-	
+
 	public void setEmptyItem(ItemStack ei) {
 		this.emptyItem = ei;
 	}
-	
+
 	public boolean doesContainerItemLeaveCraftingGrid(ItemStack stack) {
 		return false;
 	}
-	
+
 	@Override
 	public ItemStack getContainerItem(ItemStack stack) {
-		int dmg = stack.getItemDamage();
-		ItemStack newStack = copyStack(stack, 1);
-		newStack.setItemDamage(dmg + 1);
-		return newStack;
+		if (ConfigHandler.matchboxDurability > 1) {
+			int dmg = stack.getItemDamage();
+			ItemStack newStack = copyStack(stack, 1);
+			newStack.setItemDamage(dmg + 1);
+			return newStack;
+		} else {
+			return stack;
+		}
 	}
-	
+
 	public static ItemStack copyStack(ItemStack stack, int n) {
 		return new ItemStack(stack.getItem(), n, stack.getItemDamage());
 	}
-	
+
 }
