800f87d3452fb81a3ac44c5c023a3150dccafe1f
ChaosTheDude <chaos.realm@yahoo.com>
2016-03-21 10:44:42 -0400

    BlockMovingLightSource is now an instance of BlockAir

--- src/main/java/com/chaosthedude/realistictorches/blocks/BlockMovingLightSource.java
+++ src/main/java/com/chaosthedude/realistictorches/blocks/BlockMovingLightSource.java
@@ -4,6 +4,7 @@ import com.chaosthedude.realistictorches.RealisticTorches;
 import com.chaosthedude.realistictorches.blocks.te.TEMovingLightSource;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockAir;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
@@ -11,56 +12,16 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
-public class BlockMovingLightSource extends Block implements ITileEntityProvider {
+public class BlockMovingLightSource extends BlockAir implements ITileEntityProvider {
 
 	public static final String NAME = "MovingLightSource";
 
 	public BlockMovingLightSource() {
-		super(Material.air);
+		super();
 		setBlockName(RealisticTorches.MODID + "_" + NAME);
 		setTickRandomly(false);
 		setLightLevel(0.9F);
 		setBlockBounds(0.5F, 0.5F, 0.5F, 0.5F, 0.5F, 0.5F);
-	}
-
-	@Override
-	public int getRenderType() {
-		return -1;
-	}
-
-	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
-		return null;
-	}
-
-	@Override
-	public boolean isOpaqueCube() {
-		return false;
-	}
-
-	@Override
-	public boolean renderAsNormalBlock() {
-		return false;
-	}
-
-	@Override
-	public boolean canPlaceBlockAt(World worldIn, int x, int y, int z) {
-		return true;
-	}
-
-	@Override
-	public void onBlockAdded(World worldIn, int x, int y, int z) {
-		return;
-	}
-
-	@Override
-	public void onNeighborBlockChange(World world, int x, int y, int z, Block neighborBlock) {
-		return;
-	}
-
-	@Override
-	public void onFallenUpon(World worldIn, int x, int y, int z, Entity entityIn, float fallDistance) {
-		return;
 	}
 
 	@Override
