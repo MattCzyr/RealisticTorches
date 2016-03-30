2e7b938ac0b3e8267e253ca6e510a590a9c2944a
ChaosTheDude <chaos.realm@yahoo.com>
2016-03-29 23:30:52 -0400

    Items (and blocks, if automatic registry is disabled) that should emit light when held can now be specified in the config

--- src/main/java/com/chaosthedude/realistictorches/util/LightSources.java
+++ src/main/java/com/chaosthedude/realistictorches/util/LightSources.java
@@ -3,25 +3,53 @@ package com.chaosthedude.realistictorches.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.chaosthedude.realistictorches.RealisticTorches;
 import com.chaosthedude.realistictorches.RealisticTorchesItems;
+import com.chaosthedude.realistictorches.config.ConfigHandler;
 
+import cpw.mods.fml.common.registry.GameData;
+import net.minecraft.block.Block;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
 public class LightSources {
 
-	public static List<Item> lightSourceList = new ArrayList<Item>() {
-		{
-			add(Items.lava_bucket);
-			add(Items.glowstone_dust);
-			add(Items.blaze_powder);
-			add(Items.blaze_rod);
-			add(RealisticTorchesItems.glowstoneCrystal);
+	public static List<Item> lightSources = new ArrayList<Item>();
+
+	public static void registerLightSources() {
+		int lightSourceBlocks = 0;
+		if (ConfigHandler.registerLightSourceBlocks) {
+			for (Object b : GameData.getBlockRegistry()) {
+				Block block = (Block) b;
+				if (block.getLightValue() > ConfigHandler.lightSourceRegistryThreshold) {
+					lightSources.add(Item.getItemFromBlock(block));
+					lightSourceBlocks++;
+				}
+			}
+		} else {
+			for (String blockName : ConfigHandler.lightSourceItems) {
+				Block block = (Block) Block.getBlockFromName(blockName);
+				if (block != null) {
+					lightSources.add(Item.getItemFromBlock(block));
+					lightSourceBlocks++;
+				}
+			}
 		}
-	};
+
+		int lightSourceItems = 0;
+		for (String itemName : ConfigHandler.lightSourceItems) {
+			Item item = (Item) Item.itemRegistry.getObject(itemName);
+			if (item != null) {
+				lightSources.add(item);
+				lightSourceItems++;
+			}
+		}
+
+		RealisticTorches.logger.info("Registered " + lightSourceBlocks + " blocks and " + lightSourceItems + " items as light sources.");
+	}
 
 	public static boolean isLightSource(Item item) {
-		return lightSourceList.contains(item);
+		return lightSources.contains(item);
 	}
 
 }
