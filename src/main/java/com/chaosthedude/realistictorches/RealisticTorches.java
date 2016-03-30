24d32185ccacb998e47e162725c44455f3c2b19d
ChaosTheDude <chaos.realm@yahoo.com>
2016-03-29 23:27:54 -0400

    1.5.4 update, clean up light source registry

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -29,14 +29,17 @@ public class RealisticTorches {
 
 	public static final String MODID = "RealisticTorches";
 	public static final String NAME = "Realistic Torches";
-	public static final String VERSION = "1.5.2";
+	public static final String VERSION = "1.5.4";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
 	@EventHandler
 	public void init(FMLPreInitializationEvent event) {
-		RealisticTorchesBlocks.mainRegistry();
-		RealisticTorchesItems.mainRegistry();
+		RealisticTorchesBlocks.init();
+		RealisticTorchesBlocks.register();
+		
+		RealisticTorchesItems.init();
+		RealisticTorchesItems.register();
 
 		ConfigHandler.loadConfig(event.getSuggestedConfigurationFile());
 
@@ -91,17 +94,7 @@ public class RealisticTorches {
 		}
 
 		GameRegistry.addRecipe(new ItemStack(Blocks.torch, 4), "x", "y", 'x', RealisticTorchesItems.glowstoneCrystal, 'y', Items.stick);
-
-		int lightSources = 0;
-		for (Object i : GameData.getBlockRegistry()) {
-			Block block = (Block) i;
-			if (block.getLightValue() > 0) {
-				LightSources.lightSourceList.add(Item.getItemFromBlock(block));
-				lightSources++;
-			}
-		}
-
-		logger.info("Registered " + lightSources + " blocks as light sources.");
+		LightSources.registerLightSources();
 	}
 
 }
