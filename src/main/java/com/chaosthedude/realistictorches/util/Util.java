f2f1420ef6ccd580b1f77a37720f499d957d89d6
ChaosTheDude <chaos.realm@yahoo.com>
2016-05-19 23:53:57 -0400

    Reorganized utils

--- src/main/java/com/chaosthedude/realistictorches/util/Util.java
+++ src/main/java/com/chaosthedude/realistictorches/util/Util.java
@@ -1,38 +1,26 @@
 package com.chaosthedude.realistictorches.util;
 
-import java.util.List;
-
 import com.chaosthedude.realistictorches.RealisticTorches;
+import com.chaosthedude.realistictorches.RealisticTorchesBlocks;
+import com.chaosthedude.realistictorches.RealisticTorchesItems;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemModelMesher;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.IRecipe;
 
 public class Util {
 
-	public static void removeRecipe(ItemStack s) {
-		int recipeCount = 0;
-		List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList();
+	public static void registerModels() {
+		ItemModelMesher modelMesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
 
-		for (int i = 0; i < recipeList.size(); i++) {
-			IRecipe currentRecipe = recipeList.get(i);
-			ItemStack output = currentRecipe.getRecipeOutput();
+		modelMesher.register(RealisticTorchesItems.matchbox, 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesItems.matchbox.NAME, "inventory"));
+		modelMesher.register(RealisticTorchesItems.glowstoneCrystal, 0, new ModelResourceLocation(RealisticTorches.MODID + ":GlowstoneCrystal", "inventory"));
 
-			if (output != null && output.getItem() == s.getItem()) {
-				recipeList.remove(i);
-				recipeCount++;
-			}
-		}
-
-		if (recipeCount == 1) {
-			RealisticTorches.logger.info("Removed " + recipeCount + " torch recipe.");
-		} else {
-			RealisticTorches.logger.info("Removed " + recipeCount + " torch recipes.");
-		}
-	}
-
-	public static ItemStack copyStack(ItemStack stack, int n) {
-		return new ItemStack(stack.getItem(), n, stack.getItemDamage());
+		modelMesher.register(Item.getItemFromBlock(RealisticTorchesBlocks.torchLit), 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesBlocks.torchLit.NAME, "inventory"));
+		modelMesher.register(Item.getItemFromBlock(RealisticTorchesBlocks.torchSmoldering), 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesBlocks.torchSmoldering.NAME, "inventory"));
+		modelMesher.register(Item.getItemFromBlock(RealisticTorchesBlocks.torchUnlit), 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesBlocks.torchUnlit.NAME, "inventory"));
 	}
 
 }
