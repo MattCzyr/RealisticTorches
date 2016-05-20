8d350519fe042ffc8beb1e403025e29c10fa9967
ChaosTheDude <chaos.realm@yahoo.com>
2016-05-19 23:54:25 -0400

    memes

--- src/main/java/com/chaosthedude/realistictorches/items/ItemMatchbox.java
+++ src/main/java/com/chaosthedude/realistictorches/items/ItemMatchbox.java
@@ -1,9 +1,12 @@
 package com.chaosthedude.realistictorches.items;
 
+import java.util.List;
+
 import com.chaosthedude.realistictorches.RealisticTorches;
 import com.chaosthedude.realistictorches.config.ConfigHandler;
-import com.chaosthedude.realistictorches.util.Util;
+import com.mojang.realmsclient.gui.ChatFormatting;
 
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -12,14 +15,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemMatchbox extends Item {
 
-	public static final String name = "Matchbox";
+	public static final String NAME = "Matchbox";
 
 	public ItemMatchbox() {
 		super();
-		setUnlocalizedName(RealisticTorches.MODID + "_" + name);
+		setUnlocalizedName(RealisticTorches.MODID + "_" + NAME);
 		setMaxStackSize(1);
 		setMaxDamage(ConfigHandler.matchboxDurability - 1);
 		setCreativeTab(CreativeTabs.tabTools);
@@ -31,20 +36,15 @@ public class ItemMatchbox extends Item {
 		if (!ConfigHandler.matchboxCreatesFire) {
 			return false;
 		}
-		
 		pos = pos.offset(side);
 
-		if (!player.canPlayerEdit(pos, side, stack)) {
-			return false;
-		} else {
-			if (world.isAirBlock(pos)) {
-				world.playSoundEffect((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
-				world.setBlockState(pos, Blocks.fire.getDefaultState());
-			}
-
-			stack.damageItem(1, player);
-			return true;
+		if (player.canPlayerEdit(pos, side, stack) && world.isAirBlock(pos)) {
+			world.playSoundEffect((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
+			world.setBlockState(pos, Blocks.fire.getDefaultState());
 		}
+
+		stack.damageItem(1, player);
+		return true;
 	}
 
 	@Override
@@ -55,12 +55,20 @@ public class ItemMatchbox extends Item {
 	@Override
 	public ItemStack getContainerItem(ItemStack stack) {
 		if (ConfigHandler.matchboxDurability > 1) {
-			ItemStack newStack = Util.copyStack(stack, 1);
+			ItemStack newStack = stack.copy();
 			newStack.setItemDamage(stack.getItemDamage() + 1);
 			return newStack;
 		}
 
 		return stack;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List info, boolean par4) {
+		if (GuiScreen.isShiftKeyDown()) {
+			info.add(ChatFormatting.ITALIC + "It's lit");
+		}
 	}
 
 }
