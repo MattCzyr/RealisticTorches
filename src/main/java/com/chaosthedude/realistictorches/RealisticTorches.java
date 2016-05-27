59714e5b9468d8399aad7aa9e749c93d6bb53a5e
ChaosTheDude <chaos.realm@yahoo.com>
2016-05-26 22:49:48 -0400

    1.5.6 update

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -33,7 +33,7 @@ import net.minecraftforge.oredict.OreDictionary;
 public class RealisticTorches {
 	public static final String MODID = "RealisticTorches";
 	public static final String NAME = "Realistic Torches";
-	public static final String VERSION = "1.5.5";
+	public static final String VERSION = "1.5.6";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
