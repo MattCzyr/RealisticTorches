9b3601e63cc4db8508346a4fb8c003bf58f1062b
ChaosTheDude <chaosthedude@gmail.com>
2016-09-03 14:17:54 -0400

    Increment version number

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class RealisticTorches {
 	public static final String MODID = "RealisticTorches";
 	public static final String NAME = "Realistic Torches";
-	public static final String VERSION = "1.6.2";
+	public static final String VERSION = "1.6.3";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
