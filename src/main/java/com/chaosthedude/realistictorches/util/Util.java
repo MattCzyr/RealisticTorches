e99c94c136e3a967109475d7179ed36958aa349e
ChaosTheDude <chaosthedude@gmail.com>
2016-09-03 14:21:31 -0400

    Automatically register item model for each object in block/item registries; add SideOnly and Nullable annotations

--- src/main/java/com/chaosthedude/realistictorches/util/Util.java
+++ src/main/java/com/chaosthedude/realistictorches/util/Util.java
@@ -1,5 +1,7 @@
 package com.chaosthedude.realistictorches.util;
 
+import javax.annotation.Nullable;
+
 import com.chaosthedude.realistictorches.RealisticTorches;
 import com.chaosthedude.realistictorches.RealisticTorchesBlocks;
 import com.chaosthedude.realistictorches.RealisticTorchesItems;
@@ -9,42 +11,49 @@ import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Util {
 
+	@SideOnly(Side.CLIENT)
 	public static void registerModels() {
-		ItemModelMesher modelMesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+		final ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
 
-		modelMesher.register(RealisticTorchesItems.matchbox, 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesItems.matchbox.NAME, "inventory"));
-		modelMesher.register(RealisticTorchesItems.glowstoneCrystal, 0, new ModelResourceLocation(RealisticTorches.MODID + ":GlowstoneCrystal", "inventory"));
+		for (String name : RealisticTorchesItems.registry.keySet()) {
+			registerModel(mesher, name, RealisticTorchesItems.registry.get(name));
+		}
 
-		modelMesher.register(Item.getItemFromBlock(RealisticTorchesBlocks.torchLit), 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesBlocks.torchLit.NAME, "inventory"));
-		modelMesher.register(Item.getItemFromBlock(RealisticTorchesBlocks.torchSmoldering), 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesBlocks.torchSmoldering.NAME, "inventory"));
-		modelMesher.register(Item.getItemFromBlock(RealisticTorchesBlocks.torchUnlit), 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + RealisticTorchesBlocks.torchUnlit.NAME, "inventory"));
+		for (String name : RealisticTorchesBlocks.registry.keySet()) {
+			registerModel(mesher, name, Item.getItemFromBlock(RealisticTorchesBlocks.registry.get(name)));
+		}
 	}
 
+	@SideOnly(Side.CLIENT)
+	public static void registerModel(ItemModelMesher mesher, String name, Item item) {
+		mesher.register(item, 0, new ModelResourceLocation(RealisticTorches.MODID + ":" + name, "inventory"));
+	}
+
+	@Nullable
 	public static EntityPlayer getClosestPlayer(World world, BlockPos pos, double distance) {
-		double d0 = -1.0D;
-		EntityPlayer player = null;
+		double minDistance = -1.0D;
+		EntityPlayer closestPlayer = null;
 
-		for (int i = 0; i < world.playerEntities.size(); i++) {
-			EntityPlayer player1 = (EntityPlayer) world.playerEntities.get(i);
+		for (EntityPlayer player : world.playerEntities) {
+			if (EntitySelectors.NOT_SPECTATING.apply(player)) {
+				final double playerDistance = player.getDistanceSq(pos.getX(), pos.getY(), pos.getZ());
 
-			if (EntitySelectors.NOT_SPECTATING.apply(player1)) {
-				double d1 = player1.getDistanceSq(pos.getX(), pos.getY(), pos.getZ());
-
-				if ((distance < 0.0D || d1 < distance * distance) && (d0 == -1.0D || d1 < d0)) {
-					d0 = d1;
-					player = player1;
+				if ((distance < 0.0D || playerDistance < distance * distance) && (minDistance == -1.0D || playerDistance < minDistance)) {
+					minDistance = playerDistance;
+					closestPlayer = player;
 				}
 			}
 		}
 
-		return player;
+		return closestPlayer;
 	}
 
 }
