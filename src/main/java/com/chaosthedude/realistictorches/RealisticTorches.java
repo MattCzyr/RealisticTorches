07c8b3930ce95a991605e04e03bee40f9e73c5cc
mattczyr <mattczyr@gmail.com>
2016-11-25 22:00:10 -0500

    Increment version number

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -30,7 +30,7 @@ public class RealisticTorches {
 
 	public static final String MODID = "realistictorches";
 	public static final String NAME = "Realistic Torches";
-	public static final String VERSION = "1.6.4";
+	public static final String VERSION = "2.0.0";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 	
