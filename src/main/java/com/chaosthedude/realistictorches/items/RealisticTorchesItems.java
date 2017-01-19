a8f7bd9ce71f6471bf2cefe4b4fe09094b0d3cde
mattczyr <mattczyr@gmail.com>
2017-01-19 00:08:04 -0500

    Add Glowstone Paste item

--- src/main/java/com/chaosthedude/realistictorches/items/RealisticTorchesItems.java
+++ src/main/java/com/chaosthedude/realistictorches/items/RealisticTorchesItems.java
@@ -10,15 +10,17 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class RealisticTorchesItems {
-	
+
 	public static final List<Item> REGISTRY = new ArrayList<Item>();
 
 	public static ItemMatchbox matchbox;
 	public static Item glowstoneCrystal;
+	public static Item glowstonePaste;
 
 	public static void register() {
 		matchbox = registerItem(new ItemMatchbox(), ItemMatchbox.NAME);
 		glowstoneCrystal = registerItem(new Item().setUnlocalizedName(RealisticTorches.MODID + ".glowstone_crystal").setCreativeTab(CreativeTabs.MATERIALS), "glowstone_crystal");
+		glowstonePaste = registerItem(new Item().setUnlocalizedName(RealisticTorches.MODID + ".glowstone_paste").setCreativeTab(CreativeTabs.MATERIALS), "glowstone_paste");
 	}
 
 	protected static <T extends Item> T registerItem(T itemType, String name) {
