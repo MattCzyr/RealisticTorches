84e8545cbc5781476f88760144036b97c71d64db
Cliff Hudson <cliff.s.hudson@gmail.com>
2018-01-13 16:01:35 -0800

    Bump version to 2.1.1 for config change

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -29,7 +29,7 @@ public class RealisticTorches {
 
 	public static final String MODID = "realistictorches";
 	public static final String NAME = "Realistic Torches";
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.1.1";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
