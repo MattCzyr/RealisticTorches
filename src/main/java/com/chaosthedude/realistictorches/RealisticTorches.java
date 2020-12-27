54eeeea43cac6e079e9ef6522b3f8c463f3f2117
Matt Czyr <mattczyr@gmail.com>
2020-12-27 16:56:59 -0500

    Finish port to 1.16.4

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -1,8 +1,5 @@
 package com.chaosthedude.realistictorches;
 
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,11 +10,9 @@ import com.chaosthedude.realistictorches.worldgen.TorchFeature;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.IFeatureConfig;
-import net.minecraft.world.gen.feature.NoFeatureConfig;
-import net.minecraft.world.gen.placement.IPlacementConfig;
-import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -30,7 +25,6 @@ public class RealisticTorches {
 
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "realistictorches";
-
 
     public RealisticTorches() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.COMMON_CONFIG);
