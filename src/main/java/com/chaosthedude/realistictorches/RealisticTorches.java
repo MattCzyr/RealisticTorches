bff36bf8f898f341b1dbeb7b944452e5bf0ea2a9
Matt Czyr <mattczyr@gmail.com>
2020-12-29 19:11:51 -0500

    Update changelog and bump version

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -29,7 +29,7 @@ public class RealisticTorches {
 
 	public static final String MODID = "realistictorches";
 	public static final String NAME = "Realistic Torches";
-	public static final String VERSION = "2.1.1";
+	public static final String VERSION = "2.1.2";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
