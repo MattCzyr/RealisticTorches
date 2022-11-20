d30a9c2e3462020ed3e43703b40c81d2cff3da6f
Matthew Czyr <mattczyr@gmail.com>
2022-11-20 15:27:04 -0500

    Fix crash during server initialization

--- src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
+++ src/main/java/com/chaosthedude/realistictorches/RealisticTorches.java
@@ -15,6 +15,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -34,7 +35,10 @@ public class RealisticTorches {
 		ConfigHandler.loadConfig(ConfigHandler.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("realistictorches-common.toml"));
 
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		eventBus.addListener(this::clientSetup);
+
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+			eventBus.addListener(this::clientSetup);
+		});
 
 		RealisticTorchesRegistry.FEATURE_REGISTRY.register(eventBus);
 		RealisticTorchesRegistry.CONFIGURED_FEATURE_REGISTRY.register(eventBus);
